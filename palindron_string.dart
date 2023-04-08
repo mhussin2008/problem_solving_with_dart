0cfbfbbd53a8a1da63f85ea44c991773542b6507
mohamed hussin <65326535+mhussin2008@users.noreply.github.com>
2023-04-08 03:01:07 +0200

    mohamed hussin my version of palindrome test
    tests a list of strings for palindrome status

--- palindron_string.dart
+++ palindron_string.dart
@@ -1,17 +1,44 @@
-//Ask the user for a string and print out whether this string is a palindrome or not.
-//A palindrome is a string that reads the same forwards and backwards.
+class char{
+  final String myString;
+  final  List<String> characters=[];
+  bool palindrome=true;
 
-import 'dart:io';
 
-void main() {
-  print("pleaes write name");
-    String? myString = stdin.readLineSync()!;
-  
-    if (myString[0] == myString[myString.length - 1]) {
-      print("name is palindrom");
-      return;
+  char(this.myString){
+    String c;
+    int i=0;
+    int lngth=myString.length;
+    for(i=0;i<lngth;i++){
+      c=myString.substring(i,i+1);
+      characters.add(c);
     }
-  
-  print("name is not palindroms");
- 
+    int l=(myString.length/2).toInt();
+    for(i=0;i<l;i++)
+      {
+        if(characters[i] !=characters[lngth-i-1]){
+          palindrome=false;
+        }
+      }
+  }
+
+  List<String> get chary => characters;
+  bool get isPalindrome=>palindrome;
+
+}
+
+void main(List<String> arguments){
+
+  List<String> phrases=['civic', 'radar','Hello', 'level', 'rotor','tattarrattat', 'kayak', 'madam'];
+
+
+  List<char> myChars=phrases.map((e) => char(e)).toList();
+ // myChars.map((e) => print(e.chary));
+  myChars.forEach((element) {print('${element.chary} and Palindrome state is ${element.palindrome}');});
+  //int t=(5/2).toInt();
+  //print(t);
+  //print(myChars[0].chary);
+
+
+
+
 }
